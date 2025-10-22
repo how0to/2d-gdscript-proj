cf67fd099041e45b8911ce007e4005262ef8ec4e
how0to <backh4999@gmail.com>
2025-10-22 21:59:44 +0300

    Revert "Added some functionality a stuff"
    This reverts commit 3e3f60c5d0a720039c88ceaafa34be0d697fece0.

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,12 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 4700.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	print("Script running")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -19,9 +18,8 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
-		print("moving")
-		velocity.x = direction * SPEED * delta
+		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
