ccdeacd666c327c1c8c8ece2efdd263312451080
how0to <backh4999@gmail.com>
2025-11-19 17:48:39 +0200

    lowkey i forgot but like kills?
    halo

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,20 +6,17 @@ const JUMP_VELOCITY = -250.0
 
 
 func _physics_process(delta: float) -> void:
-	print("Script running")
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get the input direction and handle the movement.
+	var direction := Input.get_axis("MoveLeft", "MoveRight")
 	if direction:
-		print("moving")
 		velocity.x = direction * SPEED * delta
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
