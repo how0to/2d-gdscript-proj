00e11027dd10f33a94a30a077567a73eab8fd8a0
how0to <backh4999@gmail.com>
2025-11-20 11:29:09 +0200

    adding coyote time in progress
    ya

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,15 @@
 extends CharacterBody2D
 
+@onready var AnimSprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 4700.0
 const JUMP_VELOCITY = -250.0
+var CanJump = false
 
+func Jump():
+	if CanJump:
+		velocity.y = JUMP_VELOCITY
+		CanJump = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,11 +18,15 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+		Jump()
 	# Get the input direction and handle the movement.
 	var direction := Input.get_axis("MoveLeft", "MoveRight")
 	if direction:
+		if direction == 1:
+			AnimSprite.flip_h = false
+		elif direction == -1:
+			AnimSprite.flip_h = true
+			
 		velocity.x = direction * SPEED * delta
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
