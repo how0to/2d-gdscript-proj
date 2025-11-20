f1d2be137251ee059d3f5ad47c1224ce5258650b
how0to <backh4999@gmail.com>
2025-11-20 14:02:39 +0200

    Coyote time removed cuz i dont want
    yes

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,20 +5,16 @@ extends CharacterBody2D
 const SPEED = 4700.0
 const JUMP_VELOCITY = -250.0
 var CanJump = false
-
-func Jump():
-	if CanJump:
-		velocity.y = JUMP_VELOCITY
-		CanJump = false
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		Jump()
+		velocity.y = JUMP_VELOCITY
 	# Get the input direction and handle the movement.
 	var direction := Input.get_axis("MoveLeft", "MoveRight")
 	if direction:
